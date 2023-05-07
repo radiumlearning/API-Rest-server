a5d4a2ff8da3475f578b369e16593f26723494ad
Esteban Frare <esteban.frare@radiumrocket.com>
2023-05-06 22:38:13 -0300

    BaSP: fix password error on login

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/login',
     .isAlphanumeric()
     .withMessage('Password must have only letters and numbers')
     .isLength({ min: 7 })
-    .withMessage('Name must have more than 7 letters'),
+    .withMessage('Password must have more than 7 letters'),
   (req, res) => {
     const { errors } = validationResult(req)
     if (errors.length) {
